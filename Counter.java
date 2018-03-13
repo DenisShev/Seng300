b8505214e837fcc055df5c21705c170bd50f822f
Kevin Lee <kevin.manwai.lee@gmail.com>
2018-03-12 23:09:27 -0600

    Added methods to detect declarations for classes, interface, and non-primitive data types.

--- Counter.java
+++ Counter.java
@@ -4,14 +4,20 @@
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
  
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
+import org.eclipse.jdt.core.dom.AnnotationTypeMemberDeclaration;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.Name;
 import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import org.eclipse.jdt.core.dom.SimpleType;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
  
 public class Counter{
 	public static void parse(char[] str) {
@@ -24,19 +30,26 @@ public class Counter{
 		cu.accept(new ASTVisitor() {
 			
 			// might need to use a set to count references?
-			//Set names = new HashSet();
+			Set names = new HashSet();
  
-			public boolean visit(VariableDeclarationFragment node) {
+			// Checks for Classes/interface declarations
+			public boolean visit(TypeDeclaration node) {
 				SimpleName name = node.getName();
-				
-				System.out.println(name + ". Declarations found: " );
+				System.out.println("Classes/interface: " + name);
+				return true;
+			}
+			// Checks for non-primitive type declarations
+			public boolean visit(SimpleType node) {
+				Name varType = node.getName();
+				System.out.println("Type: " + varType);
 				return false;
 			}
+			
 		});
 	}
  
 	public static void main(String[] args) throws IOException {
-		char[] test = "public class A { int i = 9;  \n int j; \n ArrayList<Integer> al = new ArrayList<Integer>(); j=1000; i = 10;}".toCharArray();
+		char[] test = "@test\n public class A { char k; \nint i = 9;  \n int j;String d; \n ArrayList<Integer> al = new ArrayList<Integer>(); j=1000;} enum C{Y, N};interface D{}".toCharArray();
 		parse(test);
 	}
 }
