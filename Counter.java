daa09d543696c82560b36da73b8d8510afe8d6cc
Kevin Lee <kevin.manwai.lee@gmail.com>
2018-03-13 21:17:07 -0600

    Removed visit(SimpleType). Added visit(VariableDeclarationFragment) using resolveBinding to find the type of each variables.

--- Counter.java
+++ Counter.java
@@ -1,59 +1,65 @@
-package counter;
-
 // Main method is used to test
-// TODO: Find Declaration types in visit method within compilation unit
-// This version currently finds the declaration VARIABLES, not types.
+// TODO: Declarations: Annotations, Enum, field, import, method, package
+// TODO: take out printing to console, add a counter to each declaration.
+// TODO: references
+// Current version only prints out the outputs into console  
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
- 
+
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
-import org.eclipse.jdt.core.dom.AnnotationTypeMemberDeclaration;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.Expression;
-import org.eclipse.jdt.core.dom.Name;
+import org.eclipse.jdt.core.dom.IVariableBinding;
 import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.SimpleType;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
  
-@SuppressWarnings("unused")                    //unused warning
 public class Counter{
 	public static void parse(char[] str) {
-		@SuppressWarnings("deprecation")
-		ASTParser parser = ASTParser.newParser(AST.JLS3);
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
+		parser.setResolveBindings(true);
 		parser.setSource(str);
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setBindingsRecovery(true);
+		
+		// setEnvironment and setUnitName is required to run resolveBindings
+		parser.setEnvironment(null, null, null, true);
+		parser.setUnitName("");
+		 
  
 		CompilationUnit cu = (CompilationUnit) parser.createAST(null);
- 
-		cu.accept(new ASTVisitor() {
-			
-			// might need to use a set to count references?
-			Set names = new HashSet();
- 
+		
+		cu.accept(new ASTVisitor() { 
+			// Doesn't work as intended
+			// Checks for annotation type declarations 
+			public boolean visit(AnnotationTypeDeclaration node) {
+				SimpleName name = node.getName();
+				System.out.println("Annotations" + name);
+				return true;
+			}
 			// Checks for Classes/interface declarations
 			public boolean visit(TypeDeclaration node) {
 				SimpleName name = node.getName();
 				System.out.println("Classes/interface: " + name);
 				return true;
 			}
-			// Checks for non-primitive type declarations
-			public boolean visit(SimpleType node) {
-				Name varType = node.getName();
-				System.out.println("Type: " + varType);
+			// Currently prints out the type and respective variable names
+			public boolean visit(VariableDeclarationFragment node) {
+				IVariableBinding binding = node.resolveBinding();
+				String types = binding.getType().getName();
+				String name = binding.getName();
+					
+				
+				System.out.printf("Variables: %s Name: %s\n",types, name);
 				return false;
-			}
-			
+			}			
 		});
 	}
  
 	public static void main(String[] args) throws IOException {
-		char[] test = "@test\n public class A { char k; \nint i = 9;  \n int j;String d; \n ArrayList<Integer> al = new ArrayList<Integer>(); j=1000;} enum C{Y, N};interface D{}".toCharArray();
+		char[] test = "@test\n public class A { char k; \nint i = 9;  \n int j;String d = \"asd\"; d.toCharArray();\n ArrayList<Integer> al = new ArrayList<Integer>(); j=1000;} enum c{Y, N};interface D{} A a = new A();".toCharArray();
 		parse(test);
 	}
 }
