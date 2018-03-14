40b33911d1f6e0f60f88e13bc093e81967a9585f
Blandon Tang <blandon.tang@live.ca>
2018-03-13 18:45:33 -0600

    Some test files I used to test with main and counter code. These files should be placed in the project folder of the eclipse workspace (beside src bin and .settings folders)

---
+++ test.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+
+public class test
+{
+	public static void main(String[]args)
+	{
+		int i = 2;
+		do {
+		 i++;
+		 System.out.println(i);
+		}
+		while (i < 10);
+	}
+
+}
+
